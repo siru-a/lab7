92be42eee9a93eafaa92444a3cea8a39757f4df7
Siru A <s1a@ucsd.edu>
2017-02-23 11:18:34 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,14 @@ $(document).ready(function() {
 	initializePage();
 });
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(like);
+	function like(event){
+		ga("send", "event", 'like', 'click');
+	};
 }
